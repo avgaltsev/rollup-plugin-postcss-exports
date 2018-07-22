458bb1fce10dbeefdde0227cdfb262a19065f01e
Anton Galtsev <avgaltsev@gmail.com>
2018-07-22 15:33:47 +0300

    Rethinked

--- index.ts
+++ index.ts
@@ -7,29 +7,11 @@ import * as postcss from "postcss";
 import {AcceptedPlugin} from "postcss";
 
 import postcssExports from "postcss-exports";
-import {NameGenerator, Scope, Exports} from "postcss-exports";
+import {NameGenerator, Scope} from "postcss-exports";
 
 import * as packageJson from "./package.json";
 
 const PLUGIN_NAME: string = packageJson.name;
-
-export interface CssTaker {
-	(css: string): void;
-}
-
-export interface Preprocessor {
-	(source: string): string | Promise<string>;
-}
-
-export interface PluginOptions {
-	include?: Glob;
-	exclude?: Glob;
-	extensions?: string[];
-	plugins?: AcceptedPlugin[];
-	takeCss?: CssTaker;
-	preprocess?: Preprocessor;
-	generateName?: NameGenerator;
-}
 
 const MODULE_NAME = "rollup-plugin-postcss-exports-module-name";
 
@@ -52,12 +34,30 @@ const MODULE_SOURCE = `export default function (classData) {
 
 const HEADER_PART = `import getClass from "${MODULE_NAME}";`;
 
-const getPart = (scope: Scope, name?: string) => {
+export interface CssTaker {
+	(css: string): void;
+}
+
+export interface Preprocessor {
+	(source: string): string | Promise<string>;
+}
+
+export interface PluginOptions {
+	include?: Glob;
+	exclude?: Glob;
+	extensions?: string[];
+	plugins?: AcceptedPlugin[];
+	takeCss?: CssTaker;
+	preprocess?: Preprocessor;
+	generateName?: NameGenerator;
+}
+
+const getExportPart = (scope: Scope) => {
 	const classes = Object.entries(scope).map(([className, classData]) => {
 		return `${className}: getClass(${JSON.stringify(classData)})`;
 	}).join(",");
 
-	return `export ${name ? `let ${name} =` : "default"} {${classes}};`;
+	return `export default {${classes}};`;
 };
 
 const dontTakeCss: CssTaker = (css) => {};
@@ -67,9 +67,10 @@ export default (pluginOptions: PluginOptions): Plugin => {
 
 	const extensions = Array.isArray(pluginOptions.extensions) ? pluginOptions.extensions : [];
 	const plugins = Array.isArray(pluginOptions.plugins) ? pluginOptions.plugins : [];
+
 	const generateName = pluginOptions.generateName;
-	const preprocess = (source: string) => Promise.resolve((typeof pluginOptions.preprocess === "function") ? pluginOptions.preprocess(source) : source);
 	const takeCss = (typeof pluginOptions.takeCss === "function") ? pluginOptions.takeCss : dontTakeCss;
+	const preprocess = (source: string) => Promise.resolve((typeof pluginOptions.preprocess === "function") ? pluginOptions.preprocess(source) : source);
 
 	const cssMap = new Map();
 
@@ -89,29 +90,27 @@ export default (pluginOptions: PluginOptions): Plugin => {
 		},
 
 		async transform(source, id) {
-			if (!filter(id as string) || !extensions.includes(path.extname(id as string))) {
+			if (!filter(id) || !extensions.includes(path.extname(id))) {
 				return null;
 			}
 
-			let exports: Exports;
+			let scope: Scope;
 
-			const processor = postcss([...plugins, postcssExports({
+			const processor = postcss(...plugins, postcssExports({
 				generateName,
 
-				takeExports(result) {
-					exports = result;
+				takeScope(result) {
+					scope = result;
 				},
-			})]);
+			}));
 
 			const preprocessed = await preprocess(source);
-			const processed = await processor.process(preprocessed, {from: id as string, to: id as string});
+			const processed = await processor.process(preprocessed, {from: id, to: id});
 
 			cssMap.set(id, processed);
 
-			const parts = Object.entries(exports.scopes).map(([name, scope]) => getPart(scope, name));
-
 			return {
-				code: [HEADER_PART, ...parts, getPart(exports.defaultScope)].join("\n"),
+				code: [HEADER_PART, getExportPart(scope)].join("\n"),
 			};
 		},
 
